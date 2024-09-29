5f3be9e4ec7327f4c26fd7d3cd50b8faf3e2dbd3
konstantinosvlachakis <dinos_tornado@hotmail.com>
2024-09-30 00:04:08 +0300

    login and register page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MessageContainer from "./components/MessageContainer";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import SendMessage from "./features/SendMessage";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/messages" element={<SendMessage />} />
+          <Route path="/connected-users" element={<MessageContainer />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
