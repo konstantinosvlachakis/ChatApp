5e555ddaf0f87670ec5efa6ea193f07e1150e26c
K.Vlachakis <dinos_tornado@hotmail.com>
2024-09-30 20:42:40 +0300

    Create login and register page (#1)
    * client side
    
    * remove unecessary code
    
    * Login page
    
    * Latest updates
    
    * update the user list page with the current users
    
    * Integrate an sql database
    
    * Update .gitignore
    
    * login and register page
    
    * correct the url
    
    ---------
    
    Co-authored-by: KonstantinosVlachakis <konstantinos.vlachakis@almaeconomics.com>

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:8000/api/login/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      if (response.ok) {
+        console.log("Login successful");
+        navigate("/connected-users"); // Redirect to your desired page
+      } else {
+        const errorData = await response.json();
+        console.error("Error during login:", errorData.error);
+      }
+    } catch (error) {
+      console.error("Error during login:", error.message);
+    }
+  };
+
+  const handleSignUpClick = () => {
+    navigate("/register");
+  };
+
+  return (
+    <div className="min-h-screen bg-cover bg-center bg-slate-500 flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-96">
+        <h2 className="text-2xl font-bold mb-4 text-center">Sign in</h2>
+        <form onSubmit={handleLogin}>
+          <TextField
+            id="username"
+            label="Username"
+            fullWidth
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            variant="outlined"
+            margin="normal"
+          />
+          <TextField
+            id="password"
+            label="Password"
+            fullWidth
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            variant="outlined"
+            margin="normal"
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            fullWidth
+            style={{ marginTop: "1rem" }}
+          >
+            Login
+          </Button>
+        </form>
+
+        <p className="text-center mt-4">
+          Don't have an account?{" "}
+          <span
+            className="text-blue-500 cursor-pointer hover:underline"
+            onClick={handleSignUpClick}
+          >
+            Sign Up
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
