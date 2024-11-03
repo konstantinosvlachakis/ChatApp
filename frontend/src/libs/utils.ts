d96eb100f945828c7af6ba5cdf583ae3e374a992
konstantinosvlachakis <dinos_tornado@hotmail.com>
2024-11-03 11:24:03 +0200

    add typescript and edit profile in frontend

--- frontend/src/libs/utils.ts
+++ frontend/src/libs/utils.ts
@@ -1,15 +1,6 @@
-// src/libs/utils.ts
+import clsx, { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-import { ClassValue } from 'clsx';
-import clsx from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-/**
- * Merges class names conditionally using clsx and tailwind-merge
- * 
- * @param inputs - A list of class values to merge
- * @returns A string of merged class names
- */
-export function cn(...inputs: ClassValue[]): string {
-    return twMerge(clsx(...inputs));
-}
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+  }
