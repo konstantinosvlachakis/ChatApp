481f3428bcd0d3e40a92f3b96be08532f7dd5255
konstantinosvlachakis <dinos_tornado@hotmail.com>
2025-01-05 19:01:14 +0200

    Update background color of LoginPage for improved aesthetics

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -63,7 +63,7 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="min-h-screen bg-cover bg-center bg-slate-500 flex items-center justify-center">
+    <div className="min-h-screen bg-cover bg-center bg-slate-100 flex items-center justify-center">
       <div className="bg-white p-8 rounded-lg shadow-lg w-96">
         <h2 className="text-2xl font-bold mb-4 text-center">Sign in</h2>
         <form onSubmit={handleLogin}>
