d5d8b8c73772d5189cb686448fd1995bac6510b0
konstantinosvlachakis <dinos_tornado@hotmail.com>
2025-01-06 00:24:22 +0200

    Refactor ChatRoom component to use EmojiPickerWrapper; update emoji selection logic and improve UI; adjust VSCode settings for new environment paths

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -91,7 +91,7 @@ const RegisterPage = () => {
   };
 
   return (
-    <div className="min-h-screen bg-cover bg-center bg-slate-500 flex items-center justify-center">
+    <div className="min-h-screen bg-cover bg-center bg-slate-100 flex items-center justify-center">
       <div className="bg-white p-8 rounded-lg shadow-lg w-96">
         <h2 className="text-2xl font-bold mb-4 text-center">Register</h2>
         <form onSubmit={handleRegister}>
