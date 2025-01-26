b50bea9af44596300f960a96781f47181e2d7901
konstantinosvlachakis <dinos_tornado@hotmail.com>
2025-01-26 23:55:15 +0200

    Update app name in ChatappConfig to reflect new module structure

--- backend/chatapp/apps.py
+++ backend/chatapp/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ChatappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chatapp'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "backend.chatapp"
