74dbf38551e4a7ef8684c46835bbd508f7b18b30
konstantinosvlachakis <dinos_tornado@hotmail.com>
2025-01-26 23:44:02 +0200

    Set DEBUG to False for production readiness in settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-qj$oesh)3^qim64zfab^5+yv8*ijqsc@qa1=0b8)%c6zfa0=u-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
