6f99536e951836c41e355c01b6493522364db537
konstantinosvlachakis <dinos_tornado@hotmail.com>
2025-01-26 22:59:16 +0200

    Update ASGI application imports to reflect new module structure

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chatapp.routing
+import backend.chatapp.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
@@ -19,7 +19,7 @@ application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
         "websocket": AuthMiddlewareStack(
-            URLRouter(chatapp.routing.websocket_urlpatterns)
+            URLRouter(backend.chatapp.routing.websocket_urlpatterns)
         ),
     }
 )
