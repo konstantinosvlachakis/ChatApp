31b25acfe9afb4e3e3b167804fbfee5ae29747ae
konstantinosvlachakis <dinos_tornado@hotmail.com>
2025-01-26 23:01:29 +0200

    Update import path for ChatConsumer in routing.py to reflect new module structure

--- backend/chatapp/routing.py
+++ backend/chatapp/routing.py
@@ -1,5 +1,5 @@
 from django.urls import re_path
-from chatapp.consumers import ChatConsumer
+from backend.chatapp.consumers import ChatConsumer
 
 websocket_urlpatterns = [
     re_path(r"ws/socket-server/(?P<room_name>\w+)/$", ChatConsumer.as_asgi()),
