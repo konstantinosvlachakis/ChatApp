f26ad7c643498637bc9fb22141dd5eaf258fa9e3
konstantinos-vlachakis-almaeconomics <konstantinos.vlachakis@almaeconomics.com>
2025-02-06 16:47:30 +0200

    Update static files directory path in Django settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -210,7 +210,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Define the static files directory
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [
-    "/app/frontend/build/static",  # Hardcoded absolute path
+    "/app/frontend/public/static",  # Hardcoded absolute path
 ]
 
 MEDIA_URL = "/media/"  # URL prefix for media files
