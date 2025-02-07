8ba8086166dd51a97d268e2f3a36b97c9aa974d6
konstantinosvlachakis <dinos_tornado@hotmail.com>
2025-02-07 12:16:31 +0200

    update STATIC_ROOT path to include 'backend' directory for static files

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -212,7 +212,10 @@ REST_FRAMEWORK = {
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Define the static files directory
+STATIC_ROOT = os.path.join(
+    BASE_DIR, "backend", "staticfiles"
+)  # Define the static files directory
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIR = os.path.join(
     Path(BASE_DIR).parent, "frontend/build/static"
