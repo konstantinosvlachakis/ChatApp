ef6cc8ba5b510d471cdb613bcaea0affe1187245
konstantinosvlachakis <dinos_tornado@hotmail.com>
2025-02-10 10:26:14 +0200

    Refactor database settings for production and development environments; update asset references in build files

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -123,7 +123,15 @@ WSGI_APPLICATION = "wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-DATABASES = {"default": dj_database_url.config()}
+if env == "production":
+    DATABASES = {"default": dj_database_url.config()}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -5,4 +5,6 @@ export const BASE_URL: string = isProduction
   ? "https://langvoyage-d3781c6fad54.herokuapp.com"
   : "http://127.0.0.1:8000";
 
-export const BASE_URL_IMG: string = `${BASE_URL}/media/`;
+export const BASE_URL_IMG: string = isProduction
+ ? `${BASE_URL}`
+ : `${BASE_URL}/media/`;
