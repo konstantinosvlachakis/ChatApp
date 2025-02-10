29f72bbae5fdddc1fd8d885b82ac7351822ea28a
konstantinosvlachakis <dinos_tornado@hotmail.com>
2025-02-10 16:25:48 +0200

    Refactor DATABASES configuration for cleaner production handling

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -145,15 +145,15 @@ WSGI_APPLICATION = "wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-if env == "production":
-    DATABASES = {"default": dj_database_url.config()}
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+# if env == "production":
+DATABASES = {"default": dj_database_url.config()}
+# else:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": BASE_DIR / "db.sqlite3",
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
