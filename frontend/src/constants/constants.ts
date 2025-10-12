0ceb1c08a79bcb4c09f8cbf316fd12f38c5a2a87
konstantinos-vlachakis-almaeconomics <konstantinos.vlachakis@almaeconomics.com>
2025-10-12 23:58:43 +0300

    Improve login page

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -3,6 +3,6 @@ const isProduction = process.env.NODE_ENV === "production";
 
 export const BASE_URL: string = isProduction
   ? "https://langvoyage-d3781c6fad54.herokuapp.com"
-  : "http://127.0.0.1:8000";
+  : "http://localhost:3000";
 
 export const BASE_URL_IMG =`${BASE_URL}`
